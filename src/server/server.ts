bed197c190e2709eeb9a0f3f0bf8519ff3c6d1f2
Thea Ueland <thea.ueland@digdir.no>
2023-07-31 12:42:43 +0200

    refactoring: camelCase and file structure
      * use camelCase instead of snake_case
      * updating the filestructure

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,11 @@
 import express, { Express } from 'express';
 
-import { index_router } from './controller/index_router';
-import { storage_router } from './controller/storage_router';
+import { index_router } from './controller/indexRouter';
+import { storage_router } from './controller/storageRouter';
 
-import { error_responder} from './middleware/error_handling';
+import { error_responder} from './middleware/errorHandling';
 
-import { run_test } from '../testing/test';
+import { run_test } from './testing/test';
 
 // ----------------------------------------------------------------------------
 const server: Express = express();
@@ -26,8 +26,7 @@ server.listen(PORT, () => {
 });
 // ----------------------------------------------------------------------------
 //run_test('get');
-//run_test('post_users');
-run_test('post_json');
+//run_test('post_json');
 //run_test('post_object');
 // ----------------------------------------------------------------------------
 export { createHttpError }
